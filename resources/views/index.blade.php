58234dbe5d38db48b03053e747bc5167ad45a8d6
timowa <timofejshhetinin@gmail.com>
2022-06-24 20:16:53 +0700

    а

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
                     </picture>
                 </div>
                 <div class="cta__info">
-                    <h1 class="title title-h1">Тимофей ге</h1>
+                    <h1 class="title title-h1">Денис чмо</h1>
                     <p class="cta__descr">Производство, подбор и ремонт  всех типов общепромышленных и крановых редукторов.</p>
                 </div>
             </div>
