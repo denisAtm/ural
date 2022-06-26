ce4b7f26f10bb7142b347299443583438578e327
Denis <atmajkin.denis@mail.ru>
2022-06-27 01:30:44 +0700

    Update footer.blade.php

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,3 +1,16 @@
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/62b8a5107b967b1179969757/1g6gk9j22';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 <footer class="footer">
     <div class="container footer__container">
         <div class="footer__left">
