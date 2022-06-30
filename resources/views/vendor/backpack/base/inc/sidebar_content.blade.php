2efd37a69fe6239ebf4422a72643c23d9636349f
Denis <atmajkin.denis@mail.ru>
2022-07-01 02:18:16 +0700

    Update sidebar_content.blade.php

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,11 +1,6 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-archive la-lg"></i> Страницы</a>
-    <ul class="nav-dropdown-items">
-        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('about-page') }}'><i class='nav-icon la la-star'></i> О нас</a></li>
-    </ul>
-</li>
+
 <li class="nav-item nav-dropdown">
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-newspaper-o la-lg"></i> Посты</a>
     <ul class="nav-dropdown-items">
