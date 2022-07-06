625d5de9f67ad226384c66c0e616ddf7904fcd60
Denis <atmajkin.denis@mail.ru>
2022-07-06 17:32:45 +0700

    Update base.php

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -73,7 +73,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<b>Урал</b>Редуктор',
+    'project_logo'   => '<b>Урал</b>редуктор',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
